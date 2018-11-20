aaba714608fa427d4db7bef39241e0399d7bd1e4
Stewart C. Russell <scruss@scruss.com>
2018-11-20 03:56:36 -0500

    Resolve: cmake script fails if build path contains space(s) (#111)
    * Update GenerateMake.sh
    
    script failed if build path contained space(s)
    
    * Update GenerateCodeLite.sh
    
    script failed if build path contained space(s)
    
    * Update GenerateVS2015.sh
    
    script failed if build path contained space(s)
    
    * Update GenerateVS2015_32bit.sh
    
    script failed if build path contained space(s)
    
    * Update GenerateVS2015_32bit.bat
    
    at martinweismann's request
    
    * Update GenerateVS2015.bat
    
    at martinweismann's request

--- cmake/GenerateVS2015_32bit.sh
+++ cmake/GenerateVS2015_32bit.sh
@@ -2,7 +2,7 @@
 
 basepath="$(cd "$(dirname "$0")" && pwd)"
 builddir="$basepath/../build_32bit"
-mkdir -p $builddir
-cd $builddir
+mkdir -p "$builddir"
+cd "$builddir"
 cmake .. -G "Visual Studio 14 2015" "$@"
-cd $basepath
+cd "$basepath"
