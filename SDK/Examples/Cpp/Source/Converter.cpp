451b8508a125cc71b16c6519fca576a5ad91b5c0
Martin Weismann <martin.weismann@autodesk.com>
2019-03-14 14:01:28 +0100

    Fix converter example

--- SDK/Examples/Cpp/Source/Converter.cpp
+++ SDK/Examples/Cpp/Source/Converter.cpp
@@ -140,8 +140,9 @@ int main(int argc, char** argv) {
 			std::cout << "Usage: " << std::endl;
 			std::cout << "Convert 3MF to STL: Converter.exe model.3mf" << std::endl;
 			std::cout << "Convert STL to 3MF: Converter.exe model.stl" << std::endl;
-			return convert(argv[1]);
+			return 0;
 		}
+		return convert(argv[1]);
 	}
 	catch (ELib3MFException &e) {
 		std::cout << e.what() << std::endl;
