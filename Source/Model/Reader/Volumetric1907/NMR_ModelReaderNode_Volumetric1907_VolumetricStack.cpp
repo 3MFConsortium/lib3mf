6f22574dfeb3e0f557097633ef113e0d0b2897b0
Alexander Oster <Alexander.Oster@autodesk.com>
2019-07-08 18:46:04 +0200

    First volumetric reader and writer

---
+++ Source/Model/Reader/Volumetric1907/NMR_ModelReaderNode_Volumetric1907_VolumetricStack.cpp
@@ -0,0 +1,77 @@
+/*++
+
+Copyright (C) 2019 3MF Consortium
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Abstract:
+NMR_ModelReaderNode_Volumetric1907_VolumetricStack.cpp covers the official 3MF volumetric extension.
+
+--*/
+
+#include "Model/Reader/Volumetric1907/NMR_ModelReaderNode_Volumetric1907_VolumetricStack.h"
+
+#include "Model/Classes/NMR_ModelConstants.h"
+#include "Model/Classes/NMR_ModelMeshObject.h"
+#include "Model/Classes/NMR_ModelResource.h"
+#include "Model/Classes/NMR_Model.h"
+
+#include "Common/NMR_StringUtils.h"
+#include "Common/NMR_Exception.h"
+#include "Common/NMR_Exception_Windows.h"
+
+namespace NMR {
+
+
+	CModelReaderNode_Volumetric1907_VolumetricStack::CModelReaderNode_Volumetric1907_VolumetricStack(_In_ CModel * pModel, _In_ PModelReaderWarnings pWarnings)
+		: CModelReaderNode(pWarnings)		
+		
+	{
+	}
+
+	void CModelReaderNode_Volumetric1907_VolumetricStack::parseXML(_In_ CXmlReader * pXMLReader)
+	{
+		// Parse name
+		parseName(pXMLReader);
+
+		// Parse attribute
+		parseAttributes(pXMLReader);
+
+		// Parse Content
+		parseContent(pXMLReader);
+	}
+	
+	
+	void CModelReaderNode_Volumetric1907_VolumetricStack::OnAttribute(_In_z_ const nfChar * pAttributeName, _In_z_ const nfChar * pAttributeValue)
+	{
+	}
+	
+	
+	void CModelReaderNode_Volumetric1907_VolumetricStack::OnNSAttribute(_In_z_ const nfChar * pAttributeName, _In_z_ const nfChar * pAttributeValue, _In_z_ const nfChar * pNameSpace)
+	{
+	}
+
+
+
+
+}
