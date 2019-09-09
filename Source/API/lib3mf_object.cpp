860ec01a6ad303db2a9d0a042b84f19b6b4fac6e
Martin Weismann <martin.weismann@autodesk.com>
2019-09-09 16:23:29 +0200

    Enable Object Thumbnails

--- Source/API/lib3mf_object.cpp
+++ Source/API/lib3mf_object.cpp
@@ -35,6 +35,7 @@ Abstract: This is a stub class definition of CObject
 #include "lib3mf_componentsobject.hpp"
 #include "lib3mf_metadatagroup.hpp"
 #include "lib3mf_slicestack.hpp"
+#include "lib3mf_attachment.hpp"
 
 // Include custom headers here.
 #include "Model/Classes/NMR_ModelMeshObject.h" 
@@ -146,6 +147,29 @@ bool CObject::IsValid ()
 	// Implemented by base class
 	throw ELib3MFInterfaceException (LIB3MF_ERROR_SHOULDNOTBECALLED);
 }
+
+void CObject::SetAttachmentAsThumbnail(IAttachment* pAttachment)
+{
+	auto pModelAttachment = object()->getModel()->findModelAttachment(pAttachment->GetPath());
+	if (!pModelAttachment) {
+		throw ELib3MFInterfaceException(LIB3MF_ERROR_ATTACHMENTNOTFOUND);
+	}
+	object()->setThumbnailAttachment(pModelAttachment, true);
+}
+
+IAttachment * CObject::GetThumbnailAttachment()
+{
+	if (object()->getThumbnailAttachment()) {
+		return new CAttachment(object()->getThumbnailAttachment());
+	}
+	return nullptr;
+}
+
+void CObject::ClearThumbnailAttachment()
+{
+	object()->clearThumbnailAttachment();
+}
+
 
 IMetaDataGroup * CObject::GetMetaDataGroup ()
 {
