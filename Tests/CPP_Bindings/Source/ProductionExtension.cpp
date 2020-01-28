a69236dbfa6e8415d75e133995228edd7d228545
Martin Weismann <martin.weismann@autodesk.com>
2020-01-28 20:34:42 +0100

    Prepare for Production Extension tests

---
+++ Tests/CPP_Bindings/Source/ProductionExtension.cpp
@@ -0,0 +1,93 @@
+/*++
+
+Copyright (C) 2019 3MF Consortium
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Abstract:
+
+UnitTest_ProductionExtension.cpp: Defines Unittests for the Production extension
+classes
+
+--*/
+
+#include "UnitTest_Utilities.h"
+#include "lib3mf_implicit.hpp"
+
+namespace Lib3MF
+{
+	class ProductionExtension : public ::testing::Test {
+	protected:
+		virtual void SetUp() {
+			model = wrapper->CreateModel();
+		}
+		virtual void TearDown() {
+			model.reset();
+		}
+	
+		PModel model;
+
+		static void SetUpTestCase() {
+			wrapper = CWrapper::loadLibrary();
+		}
+		static PWrapper wrapper;
+	};
+	PWrapper ProductionExtension::wrapper;
+
+	TEST_F(ProductionExtension, ReadInspect)
+	{
+		auto buffer = ReadFileIntoBuffer(sTestFilesPath + "/Production/" + "2ProductionBoxes.3mf");
+		auto reader3MF = model->QueryReader("3mf");
+		reader3MF->ReadFromBuffer(buffer);
+		CheckReaderWarnings(reader3MF, 0);
+	}
+
+	//TEST_F(ProductionExtension, ReadWrite)
+	//{
+	//}
+
+	//TEST_F(ProductionExtension, ReadWriteReadInspect)
+	//{
+	//}
+
+	//TEST_F(ProductionExtension, ReadModifyWrite)
+	//{
+	//}
+
+	//TEST_F(ProductionExtension, ReadModifySplitWriteReadInspect)
+	//{
+	//}
+
+	//TEST_F(ProductionExtension, ReadModifyJoinWriteReadInspect)
+	//{
+	//}
+
+	//TEST_F(ProductionExtension, SetupWrite)
+	//{
+	//}
+
+	//TEST_F(ProductionExtension, SetupWriteReadInspect)
+	//{
+	//}
+
+}
