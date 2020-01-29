da6c4fb28b92273145471ce8f9f02ac04b877e84
Felipe Roos <felipe.roos@hp.com>
2020-01-29 15:42:28 -0300

    SW3DRAM-975 first package reader entrypoint

---
+++ Include/Model/Reader/NMR_KeyStoreOpcPackageReader.h
@@ -0,0 +1,73 @@
+/*++
+
+Copyright (C) 2019 3MF Consortium
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Abstract:
+
+NMR_KeyStoreOpcPackageReader.h defines an OPC Package reader in a portable way.
+
+--*/
+
+#ifndef NMR_KEYSTOREOPCPACKAGEREADER
+#define NMR_KEYSTOREOPCPACKAGEREADER
+
+#include <memory>
+
+#include "Common/OPC/NMR_IOpcPackageReader.h"
+#include "Common/Platform/NMR_ImportStream.h"
+#include "Common/3MF_ProgressMonitor.h"
+#include "Model/Reader/NMR_ModelReaderWarnings.h"
+
+namespace NMR {
+
+	class COpcPackageReader;
+	using POpcPackageReader = std::shared_ptr<COpcPackageReader>;
+	class CKeyStore;
+	using PKeyStore = std::shared_ptr<CKeyStore>;
+
+	class CKeyStoreOpcPackageReader: public IOpcPackageReader {
+	private:
+		POpcPackageReader m_pPackageReader;
+		PKeyStore m_KeyStore;
+	protected:
+		NMR::PImportStream findKeyStoreStream();
+		void parseKeyStore(NMR::PImportStream keyStoreStream, NMR::PProgressMonitor pProgressMonitor);
+	public:
+		CKeyStoreOpcPackageReader(_In_ PImportStream pImportStream, _In_ PModelReaderWarnings pWarnings, _In_ PProgressMonitor pProgressMonitor);
+
+		// Inherited via IOpcPackageReader
+		virtual COpcPackageRelationship * findRootRelation(std::string sRelationType, nfBool bMustBeUnique) override;
+		virtual POpcPackagePart createPart(std::string sPath) override;
+		virtual nfUint64 getPartSize(std::string sPath) override;
+
+		// get Keystore
+		PKeyStore getKeyStore() const;
+	};
+
+	using PKeyStoreOpcPackageReader = std::shared_ptr<CKeyStoreOpcPackageReader>;
+
+}
+
+#endif // !NMR_KEYSTOREOPCPACKAGEREADER
---
+++ Source/Model/Reader/NMR_KeyStoreOpcPackageReader.cpp
@@ -0,0 +1,130 @@
+/*++
+
+Copyright (C) 2019 3MF Consortium
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Abstract:
+
+NMR_KeyStoreOpcPackageReader.cpp defines an OPC Package reader in a portable way.
+
+--*/
+
+#include "Common/NMR_Types.h"
+#ifndef __GNUC__
+#include <Windows.h>
+#else
+#include "Common/Platform/NMR_WinTypes.h"
+#endif
+
+#include "Model/Reader/NMR_KeyStoreOpcPackageReader.h"
+#include "Common/Platform/NMR_Platform.h"
+#include "Model/Classes/NMR_ModelConstants.h"
+#include "Model/Classes/NMR_KeyStore.h"
+#include "Common/OPC/NMR_OpcPackageReader.h"
+#include "Common/OPC/NMR_OpcPackagePart.h"
+#include "Model/Reader/NMR_ModelReader_InstructionElement.h"
+#include "Model/Reader/SecureContent085/NMR_ModelReaderNode_KeyStore.h"
+
+
+
+namespace NMR {
+	CKeyStoreOpcPackageReader::CKeyStoreOpcPackageReader(PImportStream pImportStream, PModelReaderWarnings pWarnings, PProgressMonitor pProgressMonitor)
+	{
+		m_pPackageReader = std::make_shared<COpcPackageReader>(pImportStream, pWarnings, pProgressMonitor);
+		m_KeyStore = std::make_shared<CKeyStore>();
+
+		PImportStream keyStoreStream = findKeyStoreStream();
+		if (nullptr != keyStoreStream)
+			parseKeyStore(keyStoreStream, pProgressMonitor);
+	}
+
+	COpcPackageRelationship * CKeyStoreOpcPackageReader::findRootRelation(std::string sRelationType, nfBool bMustBeUnique) {
+		return m_pPackageReader->findRootRelation(sRelationType, bMustBeUnique);
+	}
+
+	POpcPackagePart CKeyStoreOpcPackageReader::createPart(std::string sPath) {
+		return m_pPackageReader->createPart(sPath);
+	}
+
+	nfUint64 CKeyStoreOpcPackageReader::getPartSize(std::string sPath) {
+		return m_pPackageReader->getPartSize(sPath);
+	}
+
+	PKeyStore CKeyStoreOpcPackageReader::getKeyStore() const {
+		return m_KeyStore;
+	}
+
+	NMR::PImportStream CKeyStoreOpcPackageReader::findKeyStoreStream() {
+		COpcPackageRelationship * pKeyStoreRelation = m_pPackageReader->findRootRelation(PACKAGE_KEYSTORE_RELATIONSHIP_TYPE, true);
+		if (pKeyStoreRelation != nullptr) {
+			std::string sTargetPartURI = pKeyStoreRelation->getTargetPartURI();
+			POpcPackagePart pKeystorePart = m_pPackageReader->createPart(sTargetPartURI);
+			if (pKeystorePart == nullptr)
+				throw CNMRException(NMR_ERROR_OPCCOULDNOTGETKEYSTORESTREAM);
+			return pKeystorePart->getImportStream();
+		}
+		return nullptr;
+	}
+
+	void CKeyStoreOpcPackageReader::parseKeyStore(NMR::PImportStream keyStoreStream, NMR::PProgressMonitor pProgressMonitor) {
+
+		PXmlReader pXMLReader = fnCreateXMLReaderInstance(keyStoreStream, pProgressMonitor);
+		nfBool bHasModel = false;
+		NMR::PModelReaderWarnings m_pWarnings;
+		eXmlReaderNodeType NodeType;
+		// Read all XML Root Nodes
+		while (!pXMLReader->IsEOF()) {
+			if (!pXMLReader->Read(NodeType))
+				break;
+
+			// Get Node Name
+			LPCSTR pszLocalName = nullptr;
+			pXMLReader->GetLocalName(&pszLocalName, nullptr);
+			if (!pszLocalName)
+				throw CNMRException(NMR_ERROR_COULDNOTGETLOCALXMLNAME);
+
+			if (strcmp(pszLocalName, XML_3MF_ATTRIBUTE_PREFIX_XML) == 0) {
+				PModelReader_InstructionElement pXMLNode = std::make_shared<CModelReader_InstructionElement>(m_pWarnings);
+				pXMLNode->parseXML(pXMLReader.get());
+			}
+
+			// Compare with Model Node Name
+			if (strcmp(pszLocalName, XML_3MF_ELEMENT_KEYSTORE) == 0) {
+				if (bHasModel)
+					throw CNMRException(NMR_ERROR_DUPLICATEMODELNODE);
+				bHasModel = true;
+
+				//				m_pModel->setCurrentPath(m_pModel->rootPath());
+				PModelReaderNode_KeyStore pXMLNode = std::make_shared<CModelReaderNode_KeyStore>(m_KeyStore.get(), m_pWarnings);
+				pXMLNode->parseXML(pXMLReader.get());
+				/*
+				if (!pXMLNode->getHasResources())
+				throw CNMRException(NMR_ERROR_NORESOURCES);
+				if (!pXMLNode->getHasBuild())
+				throw CNMRException(NMR_ERROR_NOBUILD);*/
+			}
+
+		}
+	}
+}
