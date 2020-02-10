2a87b28c198a8c2dc1a07ed860cbb7858c984ed6
Felipe Roos <felipe.roos@hp.com>
2020-02-10 08:41:33 -0300

    forcing gtest /MD using cmake set variable

--- cmake/GenerateVS2015.sh
+++ cmake/GenerateVS2015.sh
@@ -4,5 +4,5 @@ basepath="$(cd "$(dirname "$0")" && pwd)"
 builddir="$basepath/../build"
 mkdir -p "$builddir"
 cd "$builddir"
-cmake .. -G "Visual Studio 14 2015 Win64" "$@" -Dgtest_force_shared_crt=ON
+cmake .. -G "Visual Studio 14 2015 Win64" "$@"
 cd "$basepath"
