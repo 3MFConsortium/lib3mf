4aeefa126b48edc4c303944995335c9bac9ce8e4
Martin Weismann <martin.weismann@autodesk.com>
2020-06-08 22:34:13 +0200

    Remove some compile options for PPC

--- cmake/GenerateMakeFast.sh
+++ cmake/GenerateMakeFast.sh
@@ -4,5 +4,5 @@ basepath="$(cd "$(dirname "$0")" && pwd)"
 builddir="$basepath/../build"
 mkdir -p "$builddir"
 cd "$builddir"
-cmake .. -G "Unix Makefiles" -DCMAKE_CXX_FLAGS:STRING="-march=native -m64 -Ofast -flto" "$@"
+cmake .. -G "Unix Makefiles" -DCMAKE_CXX_FLAGS:STRING="-Ofast" "$@"
 
