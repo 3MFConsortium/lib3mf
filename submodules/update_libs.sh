83f3f71838d03203402ba7508b262afa1d95ed48
gangatp <pradeep.gangatharan@autodesk.com>
2023-12-06 23:41:11 +0530

    adding platform specific libzip headers

--- submodules/update_libs.sh
+++ submodules/update_libs.sh
@@ -90,20 +90,10 @@ find "${lib_path_src}lib" -name "*win32*.c" \
 find "${lib_path_src}lib" \( -name "*unix*.c" -o -name "*stdio_named*.c" \) \
   -exec cp {} "${lib_path_dest}Source/unix" \;
 
-cd ${lib_path_src}
-mkdir build
-cd build
-cmake .. -DZLIB_INCLUDE_DIR="../../zlib" -DZLIB_LIBRARY=zlibstatic -DENABLE_COMMONCRYPTO=OFF -DENABLE_GNUTLS=OFF -DENABLE_MBEDTLS=OFF -DENABLE_OPENSSL=OFF -DENABLE_WINDOWS_CRYPTO=OFF
-cd ..
-cp ${lib_path_src}build/*.h "../${lib_path_dest}Include"
-rm -rf build
-cd "$base_path"
-
 cd "${lib_path_dest}Source"
 cmake -DPROJECT_SOURCE_DIR=${lib_path_src} -P ${lib_path_src}cmake/GenerateZipErrorStrings.cmake
 cd "$base_path"
 echo "$tag" > "$lib_path_dest${lib_name}_$tag.txt"
-
 
 # updating the googletest library
 lib_name="googletest"
