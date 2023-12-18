42259a8fd5d4c8c1343be27cc60ce29a60e5cd33
gangatp <pradeep.gangatharan@autodesk.com>
2023-12-18 19:58:31 +0530

    updating zlib

--- submodules/update_libs.sh
+++ submodules/update_libs.sh
@@ -32,3 +32,26 @@ if [[ "$1" == "fast_float" || "$update_all_libs" == true ]]; then
 
   echo "\"$tag\"" > "$lib_path_dest${lib_name}_$tag.txt"
 fi
+
+
+# Updating the zlib library
+if [[ "$1" == "zlib" || "$update_all_libs" == true ]]; then
+  lib_name="zlib"
+  lib_path_src="$base_path$lib_name/"
+
+  cd "$lib_name"
+  tag=$(git describe --contains HEAD)
+  cd "$base_path"
+
+  # Delete the destination library folder when updating
+  lib_path_dest="../Libraries/$lib_name/"
+  rm -rf "$lib_path_dest"
+  mkdir -p "$lib_path_dest"
+
+  mkdir "$lib_path_dest/Include"
+  mkdir "$lib_path_dest/Source"
+  cp ${lib_path_src}*.h "${lib_path_dest}Include"
+  cp ${lib_path_src}*.c "${lib_path_dest}Source"
+
+  echo "\"$tag\"" > "$lib_path_dest${lib_name}_$tag.txt"
+fi
