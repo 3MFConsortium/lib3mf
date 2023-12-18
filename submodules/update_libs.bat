7b18b508b91461589fabf034d645a853fdb2efba
gangatp <pradeep.gangatharan@autodesk.com>
2023-12-18 19:58:31 +0530

    updating zlib

--- submodules/update_libs.bat
+++ submodules/update_libs.bat
@@ -30,3 +30,28 @@ for %%A in ("fast_float" "") do (
         echo "!tag!" >"!lib_path_dest!!lib_name!_!tag!.txt"
     )
 )
+
+for %%A in ("zlib" "") do ( 
+    if "%update_lib%"==%%A (
+        REM updating the zlib library
+        set lib_name="zlib"
+        set lib_path_src="!base_path!!lib_name!\"
+
+        cd !lib_name!
+        set "tag="
+        for /f %%i in ('git describe --contains HEAD') do set "tag=%%i"
+
+        cd !base_path!
+        REM delete the destination library folder when updating
+        RD /S /Q "..\Libraries\!lib_name!"
+        set lib_path_dest="..\Libraries\!lib_name!\"
+        mkdir !lib_path_dest!
+
+        mkdir "!lib_path_dest!Include"
+        mkdir "!lib_path_dest!Source"
+        copy !lib_path_src!*.h "!lib_path_dest!Include" /Y
+        copy  !lib_path_src!*.c "!lib_path_dest!Source" /Y
+
+        echo "!tag!" >"!lib_path_dest!!lib_name!_!tag!.txt"
+    )
+)
