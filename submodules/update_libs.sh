e0bc2937d525638dd8fda946b081ceb47f4aa2ef
gangatp <pradeep.gangatharan@autodesk.com>
2023-12-18 22:26:34 +0530

    updating libzip to 1.10.1

--- submodules/update_libs.sh
+++ submodules/update_libs.sh
@@ -78,3 +78,51 @@ if [[ "$1" == "cpp-base64" || "$update_all_libs" == true  ]]; then
 
   echo "\"$tag\"" > "$lib_path_dest${lib_name}_$tag.txt"
 fi
+
+# Updating the libzip library
+if [[ "$1" == "libzip" || "$update_all_libs" == true ]]; then
+  lib_name="libzip"
+  lib_path_src="$base_path$lib_name/"
+
+  cd "$lib_name"
+  tag=$(git describe --contains HEAD)
+  cd "$base_path"
+
+  # Delete the destination library folder when updating
+  lib_path_dest="../Libraries/$lib_name/"
+  rm -rf "$lib_path_dest"
+  mkdir -p "$lib_path_dest"
+
+  mkdir "$lib_path_dest/Include"
+  mkdir "$lib_path_dest/Source"
+  find "${lib_path_src}lib" -name "*.h" \
+    ! -name "*crypto*" \
+    -exec cp {} "${lib_path_dest}Include" \;
+
+  find "${lib_path_src}lib" -name "*.c" \
+    ! -name "*crypto*" \
+    ! -name "*aes*" \
+    ! -name "*bzip2*" \
+    ! -name "*xz*" \
+    ! -name "*zstd*" \
+    ! -name "*win32*" \
+    ! -name "*unix*" \
+    ! -name "*stdio_named*" \
+    ! -name "*uwp*" \
+    -exec cp {} "${lib_path_dest}Source" \;
+
+  # copy windows specific files to win and unix specific files to unix folders
+  mkdir ${lib_path_dest}Source/win
+  mkdir ${lib_path_dest}Source/unix
+
+  find "${lib_path_src}lib" -name "*win32*.c" \
+    -exec cp {} "${lib_path_dest}Source/win" \;
+
+  find "${lib_path_src}lib" \( -name "*unix*.c" -o -name "*stdio_named*.c" \) \
+    -exec cp {} "${lib_path_dest}Source/unix" \;
+ 
+  cd "${lib_path_dest}Source"
+  cmake -DPROJECT_SOURCE_DIR=${lib_path_src} -P ${lib_path_src}cmake/GenerateZipErrorStrings.cmake
+  cd "$base_path"
+  echo "\"$tag\"" > "$lib_path_dest${lib_name}_$tag.txt"
+fi
