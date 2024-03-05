e242855f6671bd4dbf784bd60a66e66c8f6204e4
gangatp <pradeep.gangatharan@autodesk.com>
2024-03-05 19:32:36 +0530

    renaming version from 2.3.0-alpha to 2.3.0

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, 3MF Consortium'
 author = '3MF Consortium'
 
 # The full version, including alpha/beta/rc tags
-release = 'v2.3.0-alpha'
+release = 'v2.3.0'
 
 master_doc = 'index'
 
