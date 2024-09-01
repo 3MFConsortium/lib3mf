c8539813b02f380f5907487ea9f4eeee40ad65c2
Vijai Kumar S <vijai@vijaikumar.in>
2024-09-02 01:13:03 +0200

    update the one line abstract in golang examples

--- SDK/Examples/Go/beam_lattice.go
+++ SDK/Examples/Go/beam_lattice.go
@@ -25,7 +25,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-Abstract: An example that creates multiple components using transformations
+Abstract: Beam Lattice example
 
 Interface version: 2.3.2
 +++
