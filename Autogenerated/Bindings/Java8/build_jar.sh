32f3a44236c58844da2791adf57ae8701fe477d0
Alexander Oster <alexanderoster@users.noreply.github.com>
2026-02-12 14:42:32 +0100

    Lib3MF Java Bindings

---
+++ Autogenerated/Bindings/Java8/build_jar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+
+cd "$(dirname "$0")"
+echo "Download JNA"
+[ -f jna-5.5.0.jar ] || curl -O https://repo1.maven.org/maven2/net/java/dev/jna/jna/5.5.0/jna-5.5.0.jar
+
+echo "Compile Java Bindings"
+javac -classpath *.jar lib3mf/*
+
+echo "Create JAR"
+jar cvf lib3mf-2.4.0.jar lib3mf
